901956aad0da15b423874e88363bc843eadee9cb
unknown <h3khaira@edu.uwaterloo.ca>
2018-04-26 16:12:13 -0400

    Cropping

--- FaceBlurringScript.py
+++ FaceBlurringScript.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np 
 from random import randint
 
+def pixelnum(width,height,blurrValue):
+    Pixel_width=width//blurrValue
+    Pixel_height=height//blurrValue
+    return (max(Pixel_width,Pixel_height))
+
+def censor()
+
 filenum=randint(100,450)
 #imname='image_0'+str(filenum)+'.jpg'
 imname='Oscar.jpg'
@@ -17,13 +24,20 @@ i=1
 crop_image=[[]]
 for (x,y,w,h) in faces: #faces is a list containing bounding box coordinates and dimensions
     crop_image.append(raw_image[y:y+h,x:x+w])
+    resolution=pixelnum(faces[boxnum,3],faces[boxnum,2],20) #acquires size of the censoring pixels
+    for j in range(faces[boxnum,0],faces[boxnum,0]+faces[boxnum,2],resolution): #iterating through the bounding box 
+        for i in range(faces[boxnum,1], faces[boxnum,1]+faces[boxnum,3],resolution):             
+            pixelvalue=sum(raw_image[i:i:20,j:j+20])/400  #average of the range 
     cv2.imshow('Cropped '+str(i),crop_image[i])
     i=i+1
     cv2.rectangle(raw_image,(x,y),(x+w,y+h),(255,0,0),2)
-  
+
+
 # for boxnum in range(0,len(faces),1):
-#     for j in range(faces[boxnum,0],faces[boxnum,0]+faces[boxnum,2],1):#interating through the bounding box 
-#         for i in range(faces[boxnum,1], faces[boxnum,1]+faces[boxnum,3],1):
+#     resolution=pixelnum(faces[boxnum,3],faces[boxnum,2],20)
+#     for j in range(faces[boxnum,0],faces[boxnum,0]+faces[boxnum,2],resolution): #iterating through the bounding box 
+#         for i in range(faces[boxnum,1], faces[boxnum,1]+faces[boxnum,3],resolution):
+#             pixelvalue=sum()
 
 cv2.imshow('Face Detected',raw_image)
 cv2.waitKey(0)
